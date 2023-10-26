8f59ee7a61abec14b61e6154da4ab3403d3d444b
jimrothstein <jimrothstein@gmail.com>
2023-10-25 19:39:27 -0700

    wip

--- du_display_disk_usage.sh
+++ du_display_disk_usage.sh
@@ -1,9 +1,13 @@
 #!/bin/zsh
 #
 # Show disk usage in each directory in /home/jim
-
+#   USAGE:   sudo du <options>  <dir> | sort -r
+#
 # -d1 depth 1
 # -h  human
+# -s	summary (no files)
+# -- max-depth=3
+# -r (after sort) reverse
 sudo du -d1 -h ~/ | sort -hr
 
 echo "---- Mount points ----------"

b9a144f63e3b4d1d7b2b4ca0fedbaa6735b442df
jimrothstein <jimrothstein@gmail.com>
2025-11-30 21:56:16 -0800

    wip

---
+++ aider.sh
@@ -0,0 +1,4 @@
+# at cli, runs aider with the following provider/model
+
+OPENROUTER_API_KEY="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"
+aider --model openrouter/kwaipilot/kat-coder-pro:free

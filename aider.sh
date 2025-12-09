b93c361b01e358aa4906cdbd0dd5917000400ccb
jimrothstein <jimrothstein@gmail.com>
2025-12-08 22:11:19 -0800

    wip

--- aider.sh
+++ aider.sh
@@ -1,4 +1,12 @@
 # at cli, runs aider with the following provider/model
 
-OPENROUTER_API_KEY="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"
-aider --model openrouter/kwaipilot/kat-coder-pro:free
+# Works 
+# aider --model openrouter/kwaipilot/kat-coder-pro:free
+
+
+# You can run the Gemini 2.5 Pro Exp for free, with usage limits:
+aider --model gemini-exp
+
+# List models available from Gemini
+#aider --list-models gemini/
+

2d35c033d88ad58ea5659ba79003a32e49e23684
jimrothstein <jimrothstein@gmail.com>
2026-04-07 15:55:18 -0700

    wip

---
+++ SOURCE.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+# SOURCE.zsh
+
+
+# 22 MAR 2026
+
+# PURPOSE:  Problems with ~/.xinitrc not running and getting stuck in CAPS LOCK
+#           Run this as "SOURCE" to run .xinitrc.    Just a poor fix.
+
+echo "script to source .xinitrc"
+source ~/.xinitrc 
+
+

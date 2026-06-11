628fe3dfd1196959c72f0560f83b66dfca4dc65a
jimrothstein <jimrothstein@gmail.com>
2026-06-11 15:56:21 -0700

    wip

--- gh_actions_delete_runs.sh
+++ gh_actions_delete_runs.sh
@@ -2,12 +2,14 @@
 
 # REF:   https://blog.oddbit.com/post/2022-09-22-delete-workflow-runs/
 
-"repos/jimrohstein/docs/actions/runs/ID
-gh run list --json databaseId \    # lists by run id (multiple)
-   -q '.[].databaseId' | \ 
-    xargs -IID gh api \
+# Deletes all runs EXCEPT the most recent
+# xargs says "for every IID, run gh api .... 
+gh run list --json databaseId \
+  -q '.[1:] | .[].databaseId' | \
+  xargs -IID gh api \
     "repos/$(gh repo view --json nameWithOwner -q .nameWithOwner)/actions/runs/ID" \
     -X DELETE
 
 
-
+# Deletes ALL the runs
+#   -q '.[].databaseId' | \ 
